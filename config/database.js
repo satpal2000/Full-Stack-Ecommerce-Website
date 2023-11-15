f5e1de8284a92bdabfe571fc9184aaac514a7d43
satpal2000 <142295523+satpal2000@users.noreply.github.com>
2023-11-15 19:39:27 +0530

    Updated

--- config/database.js
+++ config/database.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
+import colors from "colors";
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  }
+};
 
-const connectDatabase  = ()=>{
-    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
-        console.log(`Mongodb Connected with server ${data.connection.host}`);
-    });
-}
-
-export default connectDatabase
+export default connectDB;
