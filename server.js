177a416ebffcf86e0514c20ee5bded7dbb31a903
satpal2000 <142295523+satpal2000@users.noreply.github.com>
2023-11-15 18:57:41 +0530

    Updated

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
