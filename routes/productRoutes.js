589544314cc4d03888d5074086c71fbed0a72268
satpal2000 <142295523+satpal2000@users.noreply.github.com>
2023-11-15 21:05:00 +0530

    Updated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import { brainTreePaymentController, braintreeTokenController, createProductController, deleteProductController, getProductController, getSingleProductController, productCategoryController, productCountController, productFiltersController, productListController, productPhotoController, realtedProductController, updateProductController } from "../controllers/productController.js";
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productCategoryController, productCountController, productFiltersController, productListController, productPhotoController, realtedProductController, updateProductController } from "../controllers/productController.js";
 import formidable from "express-formidable";
 
 const router = express.Router();
@@ -41,12 +41,5 @@ router.get("/related-product/:pid/:cid",realtedProductController);
 
 //category wise product
 router.get("/product-category/:slug",productCategoryController);
-
-//payment routes
-//token
-router.get("/braintree/token", braintreeTokenController);
-
-//payments
-router.post("/braintree/payment",requireSignIn,brainTreePaymentController);
 
 export default router
